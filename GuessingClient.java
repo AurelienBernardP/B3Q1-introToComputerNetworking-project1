1577269f01812e0a3bfb385311c80447dbf0ee7c
aurelienbernard <aurelienbernard@student.uliege.be>
2019-10-17 10:06:59 +0200

    compiles but there are still exceptions to handle

--- GuessingClient.java
+++ GuessingClient.java
@@ -6,27 +6,29 @@ class GuessingClient {
         
         if(argv.length != 3){
             System.out.println("bad number of arguments given");
+            return;
         }
-
+        Socket clientSocket;
         try{
-            Socket clientSocket = new Socket(Integer.parseInt(argv[1]), Integer.parseInt(argv[2]));
+            clientSocket = new Socket("localhost", Integer.parseInt(argv[2]));
         }catch( Exception e){
             System.out.println("ERROR : socket could not be initialized");
-            clientSocket.close();
             return;
         }
 
         try{
-            socket.setTcpNoDelay(true);
+            clientSocket.setTcpNoDelay(true);
         }catch(SocketException e){
             System.out.println("ERROR : socket could not be set to no delay mode");
             clientSocket.close();
             return;
         }
 
+        OutputStream outStream;
+        InputStream inStream;
         try{
-            OutputStream outStream = clientSocket.getOutputStream();
-            InputStream inStream = clientSocket.getInputStream();
+            outStream = clientSocket.getOutputStream();
+            inStream = clientSocket.getInputStream();
         } catch(IOException e){
             System.out.println("ERROR : Data streams could not be initialized");
             clientSocket.close();
@@ -34,7 +36,7 @@ class GuessingClient {
         }
 
         try{
-            outStream.write(new string(argv[1] + argv[2] + "\n\r").getBytes());
+            outStream.write(new String(argv[1] + argv[2] + "\n\r").getBytes());
             outStream.flush();
         }catch(IOException e){
             System.out.println("ERROR : Data streams could not be sent to tester server");
@@ -42,8 +44,9 @@ class GuessingClient {
             return;
         }
 
+        BufferedReader socketReader;
         try{
-            BufferedReader socketReader = BufferedReader(inStream);
+            socketReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         }
         catch(IllegalArgumentException e){
@@ -51,9 +54,9 @@ class GuessingClient {
             clientSocket.close();
             return;
         }
-
+        String message;
         try{
-        String message = socketReader.readLine();
+            message = socketReader.readLine();
         }
         catch(IOException IOe){
             System.out.println("ERROR : network message could not be read");
@@ -64,6 +67,7 @@ class GuessingClient {
         if(! message.equals("OK\n\r")){
             System.out.println("ERROR : connection to tester could not be established");
         }
+        
         System.out.println("Connection to tester was established");
         clientSocket.close();
     }
